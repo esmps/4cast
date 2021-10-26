3927ef0e36eb11862458c6ce970497fa8b488e15
Emma <emma.pines.s@gmail.com>
2021-10-25 19:15:36 -0700

    created WTForms templates

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, Optional
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    first_name =StringField('First Name', validators=[DataRequired()])
+    last_name =StringField('Last Name', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    home_location = StringField('Home Location')
+    daily_emails = BooleanField('Daily Weather Updates')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6), DataRequired()])
+
+
+class EditProfileForm(FlaskForm):
+    """Form to edit user profile"""
+
+    first_name =StringField('First Name', validators=[Optional()])
+    last_name =StringField('Last Name', validators=[Optional()])
+    email = StringField('E-mail', validators=[Email(), Optional()])
+    home_location = StringField('Home Location')
+    password = PasswordField('Password', validators=[DataRequired()])
