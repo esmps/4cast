315b05532cd83da8b602060812f41763076ef21e
Emma <emma.pines.s@gmail.com>
2021-10-25 19:25:06 -0700

    added more locations and fixed user signup

--- seed.py
+++ seed.py
@@ -7,11 +7,13 @@ from models import User, Location
 db.drop_all()
 db.create_all()
 
-user1 = User(first_name="Emma", last_name="Pines", email="emma.pines.s@gmail.com", password="test123", home_location="Santa Clara, CA", daily_emails=True)
-user2 = User(first_name="Emma", last_name="Schwartz", email="test@test.com", password="test123", home_location="Oakland, CA", daily_emails=False)
+user1 = User.signup(first_name="Emma", last_name="Pines", email="emma.pines.s@gmail.com", password="test123", home_location="Santa Clara, CA", daily_emails=True)
+user2 = User.signup(first_name="Emma", last_name="Schwartz", email="test@test.com", password="test123", home_location="Oakland, CA", daily_emails=False)
 
 location1 = Location(user_id=1, location="San Francisco, CA")
 location2 = Location(user_id=1, location="South Lake Tahoe, CA")
+location3 = Location(user_id=2, location="Austin, TX")
+location4 = Location(user_id=2, location="London")
 
-db.session.add_all([user1, user2, location1, location2])
+db.session.add_all([location1, location2, location3, location4])
 db.session.commit()
