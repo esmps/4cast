0944f351e887be1d47bb5a6ca4c7103e5f5da333
Emma <emma.pines.s@gmail.com>
2021-11-09 19:00:57 -0800

    removed daily_email, replaced with c_or_f

--- seed.py
+++ seed.py
@@ -7,8 +7,8 @@ from models import User, Location
 db.drop_all()
 db.create_all()
 
-user1 = User.signup(first_name="Emma", last_name="Pines", email="emma.pines.s@gmail.com", password="test123", home_location="Santa Clara, CA", daily_emails=True)
-user2 = User.signup(first_name="Emma", last_name="Schwartz", email="test@test.com", password="test123", home_location="Oakland, CA", daily_emails=False)
+user1 = User.signup(first_name="Emma", last_name="Pines", email="emma.pines.s@gmail.com", password="test123", home_location="Santa Clara, CA", c_or_f="C")
+user2 = User.signup(first_name="Emma", last_name="Schwartz", email="test@test.com", password="test123", home_location="Oakland, CA", c_or_f="F")
 
 location1 = Location(user_id=1, location="San Francisco, CA")
 location2 = Location(user_id=1, location="South Lake Tahoe, CA")
