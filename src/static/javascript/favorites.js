7a46febeadf82e8b8504dc706c59b3e5c9d86769
Emma <emma.pines.s@gmail.com>
2021-11-15 18:43:12 -0800

    fixed target being removed

--- src/static/javascript/favorites.js
+++ src/static/javascript/favorites.js
@@ -6,32 +6,32 @@ async function removeFav(target, id){
 
 async function addFav(location){
     await axios.post(`${BASE_URL}/favorite/${location}`);
+    window.location.reload();
 }
 
 // Remove favorite location from homepage
 $("#weather-card").on("click", ".remove-fav", async function (evt){
     evt.preventDefault();
     const weatherCard = $(evt.target).parent().parent().parent().parent();
-    const weatherCardID = weatherCard.attr("data-location-id")
-    removeFav(weatherCard, weatherCardID)
-    target.remove()
-    window.location.reload()
+    const weatherCardID = weatherCard.attr("data-location-id");
+    removeFav(weatherCard, weatherCardID);
+    weatherCard.remove();
+    window.location.reload();
 })
 
 // Remove favorite location from search page
 $("#search-weather-card").on("click", ".remove-fav", async function (evt){
     evt.preventDefault();
     const weatherCard = $(evt.target).parent().parent().parent().parent();
-    const weatherCardID = weatherCard.attr("data-location-id")
-    removeFav(weatherCard, weatherCardID)
-    window.location.reload()
+    const weatherCardID = weatherCard.attr("data-location-id");
+    removeFav(weatherCard, weatherCardID);
+    window.location.reload();
 })
 
 // Add favorite location from search page
 $("#search-weather-card").on("click", ".add-fav", async function (evt){
     evt.preventDefault();
     const weatherCard = $(evt.target).parent().parent().parent().parent();
-    const location = weatherCard.attr("data-location-name")
-    addFav(location)
-    window.location.reload()
+    const location = weatherCard.attr("data-location-name");
+    addFav(location);
 })
