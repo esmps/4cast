c63cfa2253b7a9811422c2fda425f7cc8984a751
Emma <emma.pines.s@gmail.com>
2021-11-17 15:20:25 -0800

    updated formatting

--- src/app.py
+++ src/app.py
@@ -87,17 +87,12 @@ extendApp_newsfeed(app)
 
 ##############################################################################
 # Turn off all caching in Flask
-#   (useful for dev; in production, this kind of stuff is typically
-#   handled elsewhere)
-#
-# https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
 
 @app.after_request
 def add_header(req):
     """Add non-caching headers on every request."""
 
-    req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
     req.headers["Pragma"] = "no-cache"
     req.headers["Expires"] = "0"
-    req.headers['Cache-Control'] = 'public, max-age=0'
     return req
