0a95e71cf4131d6cbd8abfccb94b29feaf24387b
Emma <emma.pines.s@gmail.com>
2021-11-16 22:23:16 -0800

    added app.debug

--- src/app.py
+++ src/app.py
@@ -19,6 +19,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+app.debug = False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
