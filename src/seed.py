e947e7895b84fa1d55a49d6efdfd9117f36ae436
Emma <emma.pines.s@gmail.com>
2021-11-16 18:41:35 -0800

    updated seed file

--- src/seed.py
+++ src/seed.py
@@ -1,19 +1,8 @@
 """Seed database with sample data"""
 
-from app import db
-from models import User, Location
+from app import app
+from models import db, User, Location
 
 
 db.drop_all()
 db.create_all()
-
-user1 = User.signup(first_name="Emma", last_name="Pines", username="esmps", password="test123", home_location="Santa Clara, CA", c_or_f="c")
-user2 = User.signup(first_name="Emma", last_name="Schwartz", username="test123", password="test123", home_location="Oakland, CA", c_or_f="f")
-
-location1 = Location(user_id=1, location="Santa Clara, CA")
-location2 = Location(user_id=1, location="South Lake Tahoe, CA")
-location3 = Location(user_id=2, location="Oakland, CA")
-location4 = Location(user_id=2, location="London")
-
-db.session.add_all([location1, location2, location3, location4])
-db.session.commit()
